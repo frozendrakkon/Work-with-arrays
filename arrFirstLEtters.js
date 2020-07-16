ad7f6830acd2ed4ed8bd2c357f2e97b9c499c90d
sega <frozendrakon@gmail.com>
2020-07-16 21:07:15 +0300

    Check this Task SEGA

---
+++ arrFirstLEtters.js
@@ -0,0 +1,13 @@
+const arrFirstLetters = ((str) => {  
+  const conditionFilter = ((curElement, index, array) => {
+    if(index === 0) {
+      return true
+    } else 
+      return (array[index - 1] === " ");
+  })
+  const result = [].filter.call(str, conditionFilter)
+  return result
+  
+})
+console.log(arrFirstLetters("Каждый охотник желает знать, где сидит фазан."));
+
